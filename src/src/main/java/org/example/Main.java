3afea84d2a211160c7c3188919261a06170bab5e
Santiago <santiago.sgm12@gmail.com>
2025-01-21 14:20:20 +0100

    añadido utf8 - main

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
     }
 }
