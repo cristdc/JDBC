99ad98f0f47ff3084bbca0ecceb1a3bc27a46ec8
Santiago <santiago.sgm12@gmail.com>
2025-01-27 08:34:22 +0100

    Revert "añadido utf8 - main"
    This reverts commit 3afea84d2a211160c7c3188919261a06170bab5e.

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
