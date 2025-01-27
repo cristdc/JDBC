48bfce1451e757ef838505dc7dd2876914e41c80
Santiago <santiago.sgm12@gmail.com>
2025-01-27 08:30:45 +0100

    conexión base de datos establecida

--- src/src/main/java/org/example/modelo/Main.java
+++ src/src/main/java/org/example/modelo/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.modelo;
 
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
@@ -6,5 +6,6 @@ import java.io.UnsupportedEncodingException;
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException {
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        ConexionSingleton.getConexion();
     }
 }
